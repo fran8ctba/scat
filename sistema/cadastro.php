8dc9fbb149697e2a3ce5e5ed3526dcc9d1b1b7f6
Francieli Ferreira <fran8ctba@hotmail.com>
2018-11-03 19:49:14 -0300

    tela login

---
+++ sistema/cadastro.php
@@ -0,0 +1,31 @@
+<?php 
+ 
+$login = $_POST['login'];
+$senha = MD5($_POST['senha']);
+$connect = mysqli_connect("localhost","root","","scat_db");
+
+$query_select = "SELECT login FROM usuarios WHERE login = '$login'";
+$select = mysqli_query($connect, $query_select);
+$array = mysqli_fetch_array($select);
+$logarray = $array['login'];
+ 
+  if($login == "" || $login == null){
+    echo"<script language='javascript' type='text/javascript'>alert('O campo login deve ser preenchido');window.location.href='../index.html';</script>";
+ 
+    }else{
+      if($logarray == $login){
+ 
+        echo"<script language='javascript' type='text/javascript'>alert('Esse login já existe');window.location.href='../index.html';</script>";
+        die();
+ 
+      }else{
+        $query = "INSERT INTO usuarios (login,senha) VALUES ('$login','$senha')";
+        $insert = mysqli_query($connect, $query);
+         
+        if($insert){
+          echo"<script language='javascript' type='text/javascript'>alert('Usuário cadastrado com sucesso!');window.location.href='../index.html'</script>";
+        }else{
+          echo"<script language='javascript' type='text/javascript'>alert('Não foi possível cadastrar esse usuário');window.location.href='../index.html'</script>";
+        }
+      }
+    }
---
+++ system/comum.php
@@ -0,0 +1,3 @@
+<?php 
+// Conecta-se com o MySQL 
+$link = mysqli_connect("localhost", "root", "","scat_db"); 
